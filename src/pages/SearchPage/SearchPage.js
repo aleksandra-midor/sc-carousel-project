e8326edbbf4753da2bb3f56351b89b358ac91f22
octane\ola <ola_midor@outlook.com>
2020-09-12 10:43:32 +0200

    search input added

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SearchPage = () => {
+
+const [searchInput, setSearchInput] = useState('');
+
   return (
     <main className='SearchPage'>
       <h1>Search Page</h1>
+    <label>
+      What are you looking for?
+      <input 
+      value={searchInput}
+      type='text'
+      onChange={(event) => {
+        setSearchInput(event.target.value)
+      }}
+      >
+      </input>
+    </label>
+    <p>{searchInput}</p>
+    
     </main>
+    
   )
 }
 export default SearchPage
